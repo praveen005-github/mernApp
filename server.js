097e1b1268fa185ebaaf6aaf8b53eea68f4ec5ab
Praveen <Attractivepraveen25@gmail.com>
2021-03-04 11:59:36 +0530

    Added Hello

--- server.js
+++ server.js
@@ -28,10 +28,9 @@ app.use(express.urlencoded({extended : false}));
 app.use(cors());
 //HTTP request logger
 app.use(morgan('tiny'));
-app.use('/',routes);
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'));
 }
-
+app.use('/',routes);
 app.listen(PORT, console.log(`Server is running at ${PORT}`))
