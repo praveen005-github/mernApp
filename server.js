15f11d6f1aefbe2426cd3c064d3f85ea54819413
Praveen <Attractivepraveen25@gmail.com>
2021-03-04 11:44:20 +0530

    Added Hello

--- server.js
+++ server.js
@@ -30,15 +30,8 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use('/',routes);
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
+if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+}
 
 app.listen(PORT, console.log(`Server is running at ${PORT}`))
